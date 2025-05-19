b5e9bb1529fae26506885521a21d17d1b5941092
olafin18 <olafmiramontest@gmail.com>
2025-05-19 11:20:07 -0600

    Agregar archivos de proyecto.

---
+++ App_Cliente_ejemplo/Module1.vb
@@ -0,0 +1,12 @@
+﻿
+Module modMain
+
+    Sub Main()
+        Dim objCOMPlus As New FRAME_4_7_2.COMPlusServices()
+
+        Console.WriteLine(objCOMPlus.DoTransaction())
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ App_Cliente_ejemplo/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("App_Cliente_ejemplo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("App_Cliente_ejemplo")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("e0e3ef5f-e31f-4af2-a94f-99ed5bb75485")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
